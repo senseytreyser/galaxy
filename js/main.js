080871ced7aef6db5d0a7f03be760736daf2def7
Senseytreyser <senseytreyser@mail.ru>
2019-05-25 15:59:22 +0300

    добавление анимации

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 ﻿window.onload = function() {
-    var navSide = document.getElementById("nav-1");
     var secondNavSide = document.getElementById("nav-2");
 
     document.addEventListener("click", handleClick);
 
     function handleClick(e){
-        console.log(e.target);
         var target = e.target;
 
         if (target.classList.contains("nav__link") && !target.classList.contains("open")){
@@ -17,7 +15,7 @@
             secondNavSide.classList.remove("open");
             return
         }
-        
+
         return;
     }
 }
