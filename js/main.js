dc6c20ad0572d49d0308d2d704dd5bca23c75380
Senseytreyser <senseytreyser@mail.ru>
2019-05-25 15:50:14 +0300

    Версия для компьютеров

---
+++ js/main.js
@@ -0,0 +1,23 @@
+﻿window.onload = function() {
+    var navSide = document.getElementById("nav-1");
+    var secondNavSide = document.getElementById("nav-2");
+
+    document.addEventListener("click", handleClick);
+
+    function handleClick(e){
+        console.log(e.target);
+        var target = e.target;
+
+        if (target.classList.contains("nav__link") && !target.classList.contains("open")){
+            secondNavSide.classList.add("open");
+            return;
+        } 
+        
+        if(secondNavSide.classList.contains("open") && !target.closest("#nav-2")){
+            secondNavSide.classList.remove("open");
+            return
+        }
+        
+        return;
+    }
+}
